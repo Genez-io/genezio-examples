f8403a9f121b26694b3c92875d03eb62531b0abe
Virgil993 <turcu.virgil@gmail.com>
2023-10-27 12:36:02 +0300

    Revert "qr generator example added"
    This reverts commit 075ddd57d798bce162a1b0a170653f62cd48747b.

--- kotlin/qr-generator/client/app/build/generated/data_binding_trigger/debug/com/example/qrgeneratorfrontend/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.example.qrgeneratorfrontend;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
--- kotlin/qr-generator/client/app/build/generated/source/kapt/debug/com/example/qrgeneratorfrontend/BR.java
+++
@@ -1,7 +0,0 @@
-package com.example.qrgeneratorfrontend;
-
-public class BR {
-  public static final int _all = 0;
-
-  public static final int viewModelUser = 1;
-}
