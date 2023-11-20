56f0da3d1e2d00cbd757f9f8a800cbc81ebe5f06
Virgil993 <turcu.virgil@gmail.com>
2023-11-20 16:34:20 +0200

    to do javascript done

--- javascript/todo-list/server/task.js
+++ javascript/todo-list/server/task.js
@@ -1,8 +1,11 @@
-import { mongoose } from "mongoose"
-import { reqAuth, MONGO_DB_URI } from "./helper"
-import { TaskModel } from "./models/task"
-import { GenezioDeploy } from "@genezio/types"
+import { mongoose } from "mongoose";
+import { reqAuth } from "./helper";
+import { TaskModel } from "./models/task";
+import { GenezioDeploy } from "@genezio/types";
 
+const red_color = "\x1b[31m%s\x1b[0m";
+const missing_env_error =
+  "ERROR: Your MONGO_DB_URI environment variable is not properly set, go to https://genez.io/blog/how-to-add-a-mongodb-to-your-genezio-project/ to learn how to integrate your project with Mongo DB";
 
 /**
  * The Task server class that will be deployed on the genezio infrastructure.
@@ -17,65 +20,98 @@ export class Task {
    * Private method used to connect to the DB.
    */
   #connect() {
-    mongoose.connect(MONGO_DB_URI);
+    if (!process.env.MONGO_DB_URI) {
+      console.log(red_color, missing_env_error);
+      return;
+    }
+    mongoose.connect(process.env.MONGO_DB_URI || "").catch((err) => {
+      console.log(err);
+      throw err;
+    });
   }
 
   /**
    * Method that returns all tasks for a giving user ID.
    * Only authenticated users with a valid token can access this method.
-   * 
+   *
    * The method will be exported via SDK using genezio.
-   * 
+   *
    * @param {*} token The user's token.
    * @param {*} userId The user ID.
    * @returns An object containing two properties: { success: true, tasks: tasks }
    */
   async getAllTasksByUser(token, userId) {
-    console.log(`Get all tasks by user request received with userID ${userId}`)
+    if (!process.env.MONGO_DB_URI) {
+      console.log(red_color, missing_env_error);
+      return { success: false, err: missing_env_error };
+    }
+    console.log(`Get all tasks by user request received with userID ${userId}`);
 
     const authObject = await reqAuth(token);
     if (!authObject.success) {
       return authObject;
     }
-    const tasks = await TaskModel.find({ ownerId: userId });
+    let tasks;
+    try {
+      tasks = (await TaskModel.find({ ownerId: userId })).map((task) => ({
+        title: task.title,
+        ownerId: task.ownerId,
+        solved: task.solved,
+        date: task.date,
+        _id: task._id.toString(),
+      }));
+    } catch (error) {
+      return { success: false, tasks: [], err: error.toString() };
+    }
     return { success: true, tasks: tasks };
   }
 
   /**
    * Method that creates a task for a giving user ID.
    * Only authenticated users with a valid token can access this method.
-   * 
+   *
    * The method will be exported via SDK using genezio.
-   * 
+   *
    * @param {*} token The user's token.
    * @param {*} title The tasktitle.
    * @param {*} ownerId The owner's of the task ID.
    * @returns An object containing two properties: { success: true, tasks: tasks }
    */
   async createTask(token, title, ownerId) {
-    console.log(`Create task request received for user with id ${ownerId} with title ${title}`)
+    if (!process.env.MONGO_DB_URI) {
+      console.log(red_color, missing_env_error);
+      return { success: false, err: missing_env_error };
+    }
+    console.log(
+      `Create task request received for user with id ${ownerId} with title ${title}`
+    );
 
     const authObject = await reqAuth(token);
     if (!authObject.success) {
       return authObject;
     }
-    const task = await TaskModel.create({
-      title: title,
-      ownerId: ownerId
-    });
+    let task;
+    try {
+      task = await TaskModel.create({
+        title: title,
+        ownerId: ownerId,
+      });
+    } catch (error) {
+      return { success: false, err: error.toString() };
+    }
 
     return {
       success: true,
-      task: { title: title, ownerId: ownerId, _id: task._id.toString() }
+      task: { title: title, ownerId: ownerId, _id: task._id.toString() },
     };
   }
 
   /**
    * Method that creates a task for a giving user ID.
    * Only authenticated users with a valid token can access this method.
-   * 
+   *
    * The method will be exported via SDK using genezio.
-   * 
+   *
    * @param {*} token The user's token.
    * @param {*} id The task's id.
    * @param {*} title The task's title.
@@ -83,42 +119,59 @@ export class Task {
    * @returns An object containing two properties: { success: true }
    */
   async updateTask(token, id, title, solved) {
-    console.log(`Update task request received with id ${id} with title ${title} and solved value ${solved}`)
+    if (!process.env.MONGO_DB_URI) {
+      console.log(red_color, missing_env_error);
+      return { success: false, err: missing_env_error };
+    }
+    console.log(
+      `Update task request received with id ${id} with title ${title} and solved value ${solved}`
+    );
 
     const authObject = await reqAuth(token);
     if (!authObject.success) {
       return authObject;
     }
-    await TaskModel.updateOne(
-      { _id: id },
-      {
-        title: title,
-        solved: solved
-      }
-    );
-
+    try {
+      await TaskModel.updateOne(
+        { _id: id },
+        {
+          title: title,
+          solved: solved,
+        }
+      );
+    } catch (error) {
+      return { success: false, err: error.toString() };
+    }
     return { success: true };
   }
 
   /**
    * Method that deletes a task for a giving user ID.
    * Only authenticated users with a valid token can access this method.
-   * 
+   *
    * The method will be exported via SDK using genezio.
-   * 
+   *
    * @param {*} token The user's token.
    * @param {*} title The tasktitle.
    * @param {*} ownerId The owner's of the task ID.
    * @returns An object containing one property: { success: true }
    */
   async deleteTask(token, id) {
-    console.log(`Delete task with id ${id} request received`)
+    if (!process.env.MONGO_DB_URI) {
+      console.log(red_color, missing_env_error);
+      return { success: false, err: missing_env_error };
+    }
+    console.log(`Delete task with id ${id} request received`);
 
     const authObject = await reqAuth(token);
     if (!authObject.success) {
       return authObject;
     }
-    await TaskModel.deleteOne({ _id: id });
+    try {
+      await TaskModel.deleteOne({ _id: id });
+    } catch (error) {
+      return { success: false, err: error.toString() };
+    }
 
     return { success: true };
   }
--- javascript/todo-list/server/user.js
+++ javascript/todo-list/server/user.js
@@ -1,9 +1,13 @@
-import { mongoose } from "mongoose"
-import jwt from "jsonwebtoken"
-import { UserModel } from "./models/user"
-import { ActiveSession } from "./models/activeSession"
-import { MONGO_DB_URI, validatePassword, saltPassword } from "./helper"
-import { GenezioDeploy } from "@genezio/types"
+import { mongoose } from "mongoose";
+import jwt from "jsonwebtoken";
+import { UserModel } from "./models/user";
+import { ActiveSession } from "./models/activeSession";
+import { validatePassword, saltPassword } from "./helper";
+import { GenezioDeploy } from "@genezio/types";
+
+const red_color = "\x1b[31m%s\x1b[0m";
+const missing_env_error =
+  "ERROR: Your MONGO_DB_URI environment variable is not properly set, go to https://genez.io/blog/how-to-add-a-mongodb-to-your-genezio-project/ to learn how to integrate your project with Mongo DB";
 
 /**
  * The User server class that will be deployed on the genezio infrastructure.
@@ -18,14 +22,21 @@ export class User {
    * Private method used to connect to the DB.
    */
   #connect() {
-    mongoose.connect(MONGO_DB_URI);
+    if (!process.env.MONGO_DB_URI) {
+      console.log(red_color, missing_env_error);
+      return;
+    }
+    mongoose.connect(process.env.MONGO_DB_URI || "").catch((err) => {
+      console.log(err);
+      throw err;
+    });
   }
 
   /**
    * Method that can be used to create a new user.
-   * 
+   *
    * The method will be exported via SDK using genezio.
-   * 
+   *
    * @param {*} name The user's name.
    * @param {*} email The user's email.
    * @param {*} password The user's password.
@@ -33,73 +44,112 @@ export class User {
    * is true if the creation was successfull, false otherwise.
    */
   async register(name, email, password) {
-    console.log(`Registering user with name ${name} and email ${email}...`)
+    if (!process.env.MONGO_DB_URI) {
+      console.log(red_color, missing_env_error);
+      return { success: false, err: missing_env_error };
+    }
+    console.log(`Registering user with name ${name} and email ${email}...`);
 
-    const user = await UserModel.findOne({ email: email })
+    let user;
+    try {
+      user = await UserModel.findOne({ email: email });
+    } catch (error) {
+      return { success: false, err: error.toString() };
+    }
     if (user) {
-      return { success: false, msg: "User already exists" }
+      return { success: false, msg: "User already exists" };
     } else {
-      const saltedPassword = await saltPassword(password)
-      await UserModel.create({
-        name: name,
-        email: email,
-        password: saltedPassword
-      });
+      const saltedPassword = await saltPassword(password);
+      try {
+        await UserModel.create({
+          name: name,
+          email: email,
+          password: saltedPassword,
+        });
+      } catch (error) {
+        return { success: false, err: error.toString() };
+      }
 
-      return { success: true }
+      return { success: true };
     }
   }
 
   /**
    * Method that can be used to obtain a login token for a giving user.
-   * 
+   *
    * The method will be exported via SDK using genezio.
-   * 
+   *
    * @param {*} email The user's email.
    * @param {*} password The user's password.
-   * @returns 
+   * @returns
    */
   async login(email, password) {
-    console.log(`Login request received for user with email ${email}`)
-    console.log(UserModel)
+    if (!process.env.MONGO_DB_URI) {
+      console.log(red_color, missing_env_error);
+      return { success: false, err: missing_env_error };
+    }
+    console.log(`Login request received for user with email ${email}`);
+    console.log(UserModel);
 
-    const user = await UserModel.findOne({ email: email });
+    let user;
+    try {
+      user = await UserModel.findOne({ email: email });
+    } catch (error) {
+      return { success: false, err: error.toString() };
+    }
 
     if (!user) {
       return { success: false, msg: "User not found" };
     }
 
-    const isValid = await validatePassword(user.password, password)
+    const isValid = await validatePassword(user.password, password);
 
     if (isValid) {
       user.password = null;
       const token = jwt.sign(user.toJSON(), "secret", {
-        expiresIn: 86400 // 1 week
+        expiresIn: 86400, // 1 week
       });
 
-      console.log(ActiveSession)
-      await ActiveSession.create({ token: token, userId: user._id });
-      return { success: true, user: user, token: token }
+      try {
+        await ActiveSession.create({ token: token, userId: user._id });
+      } catch (error) {
+        return { success: false, err: error.toString() };
+      }
+      return { success: true, user: user, token: token };
     } else {
-      return { success: false, msg: "Incorrect user or password" }
+      return { success: false, msg: "Incorrect user or password" };
     }
   }
 
   /**
    * Methods that receives a token and confirms if it is valid or not.
-   * 
+   *
    * @param {*} token The user's token.
    * @returns An object containing a boolean property "success" which is true if the token is valid, false otherwise.
    */
   async checkSession(token) {
-    console.log("Check session request received...")
+    if (!process.env.MONGO_DB_URI) {
+      console.log(red_color, missing_env_error);
+      return { success: false, err: missing_env_error };
+    }
+    console.log("Check session request received...");
 
-    const activeSession = await ActiveSession.findOne({ token: token });
+    let activeSession;
+    try {
+      activeSession = await ActiveSession.findOne({ token: token });
+    } catch (error) {
+      return { success: false, err: error.toString() };
+    }
     if (!activeSession) {
       return { success: false };
     }
 
-    const user = await UserModel.findById(activeSession.userId);
+    let user;
+    try {
+      user = await UserModel.findById(activeSession.userId);
+    } catch (error) {
+      return { success: false, err: error.toString() };
+    }
     if (!user) {
       return { success: false };
     }
