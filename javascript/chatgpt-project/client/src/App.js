c719f19e8d8db9101259556131140e3f59aed0a7
Virgil993 <turcu.virgil@gmail.com>
2023-11-20 12:06:52 +0200

    chatgpt javascript done

--- javascript/chatgpt-project/client/src/App.js
+++ javascript/chatgpt-project/client/src/App.js
@@ -8,25 +8,37 @@ function App() {
   const [requestText, setRequestText] = useState("");
   const [isRequesting, setIsRequesting] = useState(false);
 
+  const [alertError, setAlertError] = useState("");
+
   function sendRequest(e) {
     e.preventDefault();
     setIsRequesting(true);
     GptCaller.askChatGPT(requestText)
-      .then(response => {
+      .then((response) => {
+        if (!response.success) {
+          setAlertError(
+            `Unexpected error: ${
+              response.err
+                ? response.err
+                : "Please check the backend logs in the project dashboard - https://app.genez.io."
+            }`
+          );
+          return;
+        }
         setMessages([
           {
             text: requestText,
-            isUser: true
+            isUser: true,
           },
           {
-            text: response,
-            isUser: false
-          }
+            text: response.content,
+            isUser: false,
+          },
         ]);
         setRequestText("");
         setIsRequesting(false);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
         setIsRequesting(false);
       });
@@ -39,52 +51,54 @@ function App() {
           <h4>Rephrasing app using ChatGPT, Genezio and React</h4>
         </header>
 
-        <main className="msger-chat">
-          {messages.map((message, index) => {
-            if (message.isUser) {
-              return (
-                <div className="msg right-msg" key={index}>
-                  <div className="msg-bubble">
-                    <div className="msg-info">
-                      <div className="msg-info-name">You</div>
-                    </div>
+        {alertError != "" ? (
+          <main className="msger-chat">
+            <div className="alert">{alertError}</div>
+          </main>
+        ) : (
+          <main className="msger-chat">
+            {messages.map((message, index) => {
+              if (message.isUser) {
+                return (
+                  <div className="msg right-msg" key={index}>
+                    <div className="msg-bubble">
+                      <div className="msg-info">
+                        <div className="msg-info-name">You</div>
+                      </div>
 
-                    <div className="msg-text">
-                      {message.text}
+                      <div className="msg-text">{message.text}</div>
                     </div>
                   </div>
-                </div>
-              );
-            } else {
-              return (
-                <div className="msg left-msg" key={index}>
-                  <div className="msg-bubble">
-                    <div className="msg-info">
-                      <div className="msg-info-name">ChatGPT</div>
-                    </div>
+                );
+              } else {
+                return (
+                  <div className="msg left-msg" key={index}>
+                    <div className="msg-bubble">
+                      <div className="msg-info">
+                        <div className="msg-info-name">ChatGPT</div>
+                      </div>
 
-                    <div className="msg-text">
-                      {message.text}
+                      <div className="msg-text">{message.text}</div>
                     </div>
                   </div>
-                </div>
-              );
-            }
-          })}
-        </main>
+                );
+              }
+            })}
+          </main>
+        )}
 
-        <form className="msger-inputarea" onSubmit={e => sendRequest(e)}>
+        <form className="msger-inputarea" onSubmit={(e) => sendRequest(e)}>
           <input
             type="text"
             className="msger-input"
             placeholder="Enter your message..."
             value={requestText}
-            onChange={e => setRequestText(e.target.value)}
+            onChange={(e) => setRequestText(e.target.value)}
           />
           <button
             type="submit"
             className="msger-send-btn"
-            onClick={e => sendRequest(e)}
+            onClick={(e) => sendRequest(e)}
           >
             {isRequesting ? "Sending..." : "Send"}
           </button>
