98aa1bac70bbc704b5fbe50911cd67687f76e3ff
Virgil993 <turcu.virgil@gmail.com>
2024-03-26 15:51:16 +0200

    fixed errors

--- typescript/todo-list-auth-postgres/client/src/layouts/Admin.tsx
+++ typescript/todo-list-auth-postgres/client/src/layouts/Admin.tsx
@@ -6,18 +6,16 @@ export default function Admin(props: { element: React.ReactNode }) {
   const navigate = useNavigate();
 
   React.useEffect(() => {
-    async function checkAuth() {
-      try {
-        await AuthService.getInstance().userInfo();
-      } catch (error: any) {
-        console.log(
-          `Error: message: ${error.message}, statusCode: ${error.statusCode}`
-        );
+    try {
+      AuthService.getInstance();
+    } catch (error: any) {
+      console.log(
+        `Error: message: ${error.message}, statusCode: ${error.statusCode}`
+      );
 
-        navigate("/login");
-      }
+      navigate("/login");
+      return;
     }
-    checkAuth();
   }, []);
 
   return <>{props.element}</>;
