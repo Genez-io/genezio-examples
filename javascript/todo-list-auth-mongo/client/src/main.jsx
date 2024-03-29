e36fa75a2f60384e155f6209a7736edfac69f0b9
Virgil993 <turcu.virgil@gmail.com>
2024-03-29 17:13:25 +0200

    removed tokens from client side

--- javascript/todo-list-auth-mongo/client/src/main.jsx
+++ javascript/todo-list-auth-mongo/client/src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { AuthService } from "@genezio/auth";
 
 // TODO: Add your token and region from the Genezio dashboard https://app.genez.io/dashboard
-AuthService.getInstance().setTokenAndRegion("0-3aaapbe5m5pq34v6p3sdani5oi0qcyvz", "us-east-1");
+AuthService.getInstance().setTokenAndRegion("<token>", "<region>");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
