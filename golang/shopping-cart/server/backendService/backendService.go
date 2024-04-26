aea7e71a89260ae3505583c94c476eb441d79f9a
Virgil993 <turcu.virgil@gmail.com>
2024-04-26 15:29:15 +0300

    shopping cart backend done

--- golang/shopping-cart/server/backendService/backendService.go
+++ golang/shopping-cart/server/backendService/backendService.go
@@ -1,9 +1,11 @@
 package backend
 
 import (
-	"encoding/json"
-	"net/http"
-	"strconv"
+	"context"
+	"errors"
+	"os"
+
+	"github.com/redis/go-redis/v9"
 )
 
 type Response struct {
@@ -14,38 +16,97 @@ type Response struct {
 	City    string  `json:"city"`
 }
 
+type CartItem struct {
+	Title string `json:"title"`
+	Count string `json:"count"` 
+}
+
+var ErrCartDoesNotExist = errors.New("Cart does not exist")
+var ErrItemDoesNotExist = errors.New("Item does not exist in cart")
+
 // genezio: deploy
-type BackendService struct{}
-
-func New() BackendService {
-	return BackendService{}
+type ShoppingCartService struct{
+	redis *redis.Client
+	ctx context.Context
 }
 
-func (b BackendService) Hello(name string) (string, error) {
-	client := &http.Client{}
+func New() ShoppingCartService {
+	url :=  os.Getenv("UPSTASH_REDIS_URL")
+	if(url == ""){
+		panic("It seems that UPSTASH_REDIS_URL is not set in the `.env` file. Check the documentation https://genezio.com/docs/integrations/upstash-redis to learn how to integrate with Upstash Redis into your project.")
 
-	req, err := http.NewRequest("GET", "http://ip-api.com/json/", nil)
+	}
+	opts,err := redis.ParseURL(url)
 	if err != nil {
-		return "", err
+		panic(err)
 	}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return "Hello " + name + " Failed to get the server location :(", err
+	client := redis.NewClient(opts)
+	return ShoppingCartService{
+		redis: client,
+		ctx: context.Background(),
 	}
-
-	defer resp.Body.Close()
-
-	body := Response{}
-	err = json.NewDecoder(resp.Body).Decode(&body)
-	if err != nil || body.Status != "success" {
-		return "Hello " + name + " Failed to get the server location :(", err
-	}
-	return "Hello " + name +
-		"! This response was served from " +
-		body.City + ", " +
-		body.Country +
-		" (" + strconv.FormatFloat(body.Lat, 'f', -1, 64) +
-		", " + strconv.FormatFloat(body.Lon, 'f', -1, 64) +
-		")", nil
 }
+
+// increment item count by 1 for a given item corresponding to the sessionId
+func (s ShoppingCartService) AddItemToCart(sessionId string,item string) (string, error) {
+
+	_, err := s.redis.HIncrBy(s.ctx, "cart:"+sessionId , item,1).Result()
+	if(err != nil){
+		return "Failed to add item to cart", err
+	}
+	return "Item added to cart", nil
+}
+
+func (s ShoppingCartService) GetCart(sessionId string) ([]CartItem, error) {
+
+	cart, err := s.redis.HGetAll(s.ctx, "cart:"+sessionId).Result()
+	if(err != nil){
+		return nil, err
+	}
+	items := []CartItem{}
+	for k,v := range cart {
+		items = append(items, CartItem{Title: k, Count: v})
+	}
+	return items, nil
+}
+
+func (s ShoppingCartService) RemoveItemFromCart(sessionId string,item string) (string, error) {
+	itemExists , err := s.redis.HExists(s.ctx, "cart:"+sessionId, item).Result()
+	if(err != nil){
+		return "Failed to remove item from cart", err
+	}
+	if(!itemExists){
+		return "Item does not exist in cart", ErrItemDoesNotExist
+	}
+	_, err = s.redis.HIncrBy(s.ctx, "cart:"+sessionId , item,-1).Result()
+	if(err != nil){
+		return "Failed to remove item from cart", err
+	}
+	itemCount, err := s.redis.HGet(s.ctx, "cart:"+sessionId, item).Result()
+	if(err != nil){
+		return "Failed to remove item from cart", err
+	}
+	if(itemCount == "0"){
+		_, err = s.redis.HDel(s.ctx, "cart:"+sessionId, item).Result()
+		if(err != nil){
+			return "Failed to remove item from cart", err
+		}
+	}
+	return "Item removed from cart", nil	
+}
+
+func (s ShoppingCartService) DeleteCart(sessionId string) (string, error) {
+	cartExists , err := s.redis.Exists(s.ctx, "cart:"+sessionId).Result()
+	if(err != nil){
+		return "Failed to delete cart", err
+	}
+	if(cartExists == 0){
+		return "Cart does not exist", ErrCartDoesNotExist
+	}
+	_, err = s.redis.Del(s.ctx, "cart:"+sessionId).Result()
+	if(err != nil){
+		return "Failed to delete cart", err
+	}
+	return "Cart deleted", nil
+}
+
